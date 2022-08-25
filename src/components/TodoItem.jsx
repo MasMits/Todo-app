0ba6108c92afe0c90f95ab7f3be9142a3347b1ff
Misha Mas <misha.mas22@gmail.com>
2022-08-25 11:51:32 +0300

    add: TodoItem.css

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Checkbox from "./Checkbox";
 import DeleteButton from "./DeleteButton";
+import '../styles/TodoItem.css';
 
 
-const TodoItem = (children) => {
+const TodoItem = ({children}) => {
     return (
-        <div>
+        <div className="TodoItem">
             <Checkbox/>
             <div className="title">{children}</div>
             <DeleteButton/>
