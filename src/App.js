301ed798c01df89d61892728f61f469b4d236e3a
Kostiantyn <krokotkoleshnia@gmail.com>
2022-08-25 13:19:35 +0300

    add: App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import './styles/App.css';
+import TodoInput from "./components/TodoInput";
+import TodoItem from "./components/TodoItem";
+import React, {useState} from 'react';
 
 function App() {
+const [todos, setTodos] = useState([
+      {id: 1, title:'Smth', isComplited: false},
+      {id: 2, title:'Smth2', isComplited: false},
+      {id: 3, title:'Smth3', isComplited: false},
+    ]
+)
+  const addItem = (e) => {
+    // setTodos([...todos, {
+    //   id: Date.now(),
+    //   children, //title
+    //   isComplited: false
+    // }
+  }
+
   return (
     <div className="App">
+
+      <TodoInput
+          value={todos}
+      />
+      {todos.map((todoItem) =>
+          <div
+              className = "player-wrapper"
+              key= {todoItem.id}>
+            <TodoItem todoItem={todoItem} key= {todoItem.id} />
+          </div>
+      )}
     </div>
   );
 }
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
-import './styles/TodoInput.css';
-const TodoInput = ({addItem}) => {
-    const[title, setTitle] = useState('')
+//import './styles/TodoInput.css';
+const TodoInput = () => {
+    const[title, setTitle] = useState('');
     return (
         <div className='TodoInputClass'>
             <input
             type={'text'}
             onChange = {e => setTitle(e.target.value)}
             value={title}
-            onKeyPress={e => e.key === 'Enter' && addItem(title)}
+            //onKeyPress={e => e.key === 'Enter' && addItem(title)}
             placeholder={'Add item'}
             />
         </div>
