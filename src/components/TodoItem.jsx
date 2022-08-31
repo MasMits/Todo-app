3007090e8a95796624700477cfd18f474b3f0594
Kostiantyn <krokotkoleshnia@gmail.com>
2022-08-31 12:48:57 +0300

    feat [TodoItem.jsx]: line-through style for text

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,12 +3,13 @@ import Checkbox from "./Checkbox";
 import DeleteButton from "./DeleteButton";
 import '../styles/TodoItem.css';
 
-
 const TodoItem = ({todoItem,  deleteItem, change}) => {
     return (
         <div className="TodoItem">
             <Checkbox id={todoItem.id} isCompleted={todoItem.isCompleted} change={change}/>
-            <div className="title">{todoItem.title}</div>
+            { todoItem.isCompleted ?
+                <div className="title" style={{textDecoration: 'line-through', color: 'gray'}} >{todoItem.title} </div>
+                : <div className="title">{todoItem.title} </div> }
             <DeleteButton id={todoItem.id} deleteItem={deleteItem} />
         </div>
     );
